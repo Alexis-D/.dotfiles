57b1b24dc5a9e3294da7eed3f5ba750e83806e72
Alexis Daboville <alexis.daboville@gmail.com>
2011-05-16 17:13:08 +0200

    Update luakit theme

--- .config/luakit/theme.lua
+++ .config/luakit/theme.lua
@@ -6,41 +6,41 @@ local theme = {}
 
 -- Default settings
 theme.font = "inconsolata normal 10"
-theme.fg   = "#fff"
-theme.bg   = "#000"
+theme.fg   = "#888"
+theme.bg   = "#222"
 
 -- Genaral colours
 theme.success_fg = "#0f0"
-theme.loaded_fg  = "#33AADD"
-theme.error_fg = "#FFF"
-theme.error_bg = "#F00"
+theme.loaded_fg  = "#3ad"
+theme.error_fg = "#fff"
+theme.error_bg = "#f00"
 
 -- Warning colours
-theme.warning_fg = "#F00"
-theme.warning_bg = "#FFF"
+theme.warning_fg = "#f00"
+theme.warning_bg = "#fff"
 
 -- Notification colours
 theme.notif_fg = "#444"
-theme.notif_bg = "#FFF"
+theme.notif_bg = "#fff"
 
 -- Menu colours
-theme.menu_fg                   = "#000"
-theme.menu_bg                   = "#fff"
-theme.menu_selected_fg          = "#000"
-theme.menu_selected_bg          = "#FF0"
-theme.menu_title_bg             = "#fff"
-theme.menu_primary_title_fg     = "#f00"
-theme.menu_secondary_title_fg   = "#666"
+theme.menu_fg                   = "#888"
+theme.menu_bg                   = "#222"
+theme.menu_selected_fg          = "#fff"
+theme.menu_selected_bg          = "#257"
+theme.menu_title_bg             = "#222"
+theme.menu_primary_title_fg     = "#257"
+theme.menu_secondary_title_fg   = "#fff"
 
 -- Proxy manager
 theme.proxy_active_menu_fg      = '#000'
-theme.proxy_active_menu_bg      = '#FFF'
+theme.proxy_active_menu_bg      = '#fff'
 theme.proxy_inactive_menu_fg    = '#888'
-theme.proxy_inactive_menu_bg    = '#FFF'
+theme.proxy_inactive_menu_bg    = '#fff'
 
 -- Statusbar specific
-theme.sbar_fg         = "#fff"
-theme.sbar_bg         = "#000"
+theme.sbar_fg         = "#888"
+theme.sbar_bg         = "#222"
 
 -- Downloadbar specific
 theme.dbar_fg         = "#fff"
@@ -48,22 +48,22 @@ theme.dbar_bg         = "#000"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
-theme.ibar_fg           = "#000"
-theme.ibar_bg           = "#fff"
+theme.ibar_fg           = "#888"
+theme.ibar_bg           = "#222"
 
 -- Tab label
 theme.tab_fg            = "#888"
 theme.tab_bg            = "#222"
 theme.tab_ntheme        = "#ddd"
 theme.selected_fg       = "#fff"
-theme.selected_bg       = "#000"
+theme.selected_bg       = "#257"
 theme.selected_ntheme   = "#ddd"
-theme.loading_fg        = "#33AADD"
+theme.loading_fg        = "#3ad"
 theme.loading_bg        = "#000"
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#0F0"
-theme.notrust_fg        = "#F00"
+theme.trust_fg          = "#0e0"
+theme.notrust_fg        = "#e00"
 
 return theme
 -- vim: et:sw=4:ts=8:sts=4:tw=80
