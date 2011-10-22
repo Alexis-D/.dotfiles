5f14dfce42d0407546e5081a1a258cce5a2d6589
Alexis Daboville <alexis.daboville@gmail.com>
2011-10-22 12:22:01 +0100

    added imports + aliases

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,3 +1,20 @@
 from sys import version_info
 if version_info.major >= 3:
-    from see import see
+    try:
+        from see import see
+    except:
+        pass # bad :)
+
+import copy
+import functools
+import itertools
+import math
+import operator
+from pprint import pprint
+import random
+
+# just to be able to use fn OR functools
+# may have used two imports...
+fn = functools
+it = itertools
+op = operator
