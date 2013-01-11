2f961a630bd2c29fa33e026db2a65df951f692a1
Alexis Daboville <alexis.daboville@gmail.com>
2013-01-11 14:03:01 +0100

    Removed unused imports/aliases

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,10 +1,3 @@
-from sys import version_info
-if version_info.major >= 3:
-    try:
-        from see import see
-    except:
-        pass # bad :)
-
 import collections
 import copy
 import functools
@@ -16,9 +9,3 @@ from pprint import pprint
 import random
 import re
 import sys
-
-# just to be able to use fn OR functools
-# may have used two imports...
-fn = functools
-it = itertools
-op = operator
