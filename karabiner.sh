fbaee1695102a7b7a3140a270554caf4b1fe662a
Alexis Daboville <adaboville@palantir.com>
2014-10-22 23:56:48 +0200

    jk for ubiquitous vim and ^A"

--- karabiner.sh
+++ karabiner.sh
@@ -6,4 +6,8 @@ $cli set private.shift_right_to_ctrl_a 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
+$cli set remap.vimnormal_jk_toggle 1
+/bin/echo -n .
+$cli set private.jk_to_ctrla_double_quote 1
+/bin/echo -n .
 /bin/echo
