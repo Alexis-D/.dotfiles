d32bf10041c3f534cc699c0c44cd6c190a29bd73
Alexis Daboville <alexis.daboville@gmail.com>
2015-06-05 13:07:02 +0900

    enable said bindings

--- karabiner.sh
+++ karabiner.sh
@@ -2,11 +2,19 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.vimnormal_jk_toggle 1
+/bin/echo -n .
+$cli set private.kl_to_ctrla_n 1
+/bin/echo -n .
+$cli set private.hj_to_ctrla_p 1
+/bin/echo -n .
 $cli set private.shift_right_to_ctrl_a 1
+/bin/echo -n .
+$cli set private.ui_to_ctrla_tab 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
-$cli set remap.vimnormal_jk_toggle 1
+$cli set private.cmd_enter 1
 /bin/echo -n .
 $cli set private.jk_to_ctrla_double_quote 1
 /bin/echo -n .
